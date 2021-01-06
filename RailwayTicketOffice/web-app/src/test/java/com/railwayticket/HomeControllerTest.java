aa8b0a71c0b870089bcca90cecacd66f5c41d421
Tim01Bro <arthaos14@gmail.com>
2021-01-06 18:24:34 +0300

    Final configure ci\cd plugins

---
+++ RailwayTicketOffice/web-app/src/test/java/com/railwayticket/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package com.railwayticket;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class HomeControllerTest extends TestCase {
+
+    @Test
+    public void testHomePage() {
+    }
+
+    @Test
+    public void testListTrainPage() {
+    }
+
+    @Test
+    public void testListClientPage() {
+    }
+
+    @Test
+    public void testOneTrainPage() {
+    }
+
+    @Test
+    public void testResultFindTrainPage() {
+    }
+}
---
+++ RailwayTicketOffice/web-app/src/test/java/com/railwayticket/Test1.java
@@ -0,0 +1,10 @@
+package com.railwayticket;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void test(){
+
+    }
+}
