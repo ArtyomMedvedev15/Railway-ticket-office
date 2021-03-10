24d45186b2def253aca83f28d28e1fc2c4958d71
Tim01Bro <arthaos14@gmail.com>
2021-03-11 01:43:29 +0300

    Update client from validate

--- RailwayTicketOffice/railwayticket-angular/al-frontend/frontend/src/app/update-client/update-client.component.ts
+++ RailwayTicketOffice/railwayticket-angular/al-frontend/frontend/src/app/update-client/update-client.component.ts
@@ -3,6 +3,7 @@ import {ClientsService} from "../clients.service";
 import {Clientrailway} from "../clientrailway";
 import {ActivatedRoute, Router} from "@angular/router";
 import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-update-client',
@@ -13,9 +14,20 @@ export class UpdateClientComponent implements OnInit {
 
   clientUpdate:Clientrailway = new Clientrailway();
 
+  updateClient: FormGroup;
+
   constructor(private clientService:ClientsService,private router:ActivatedRoute,private route:Router,
    @Inject(MAT_DIALOG_DATA) public data: any,
-  private dialog:MatDialog) { }
+  private dialog:MatDialog,private formBuilder: FormBuilder) {
+
+    const MOBILE_PATTERN = '[\+]?[(]?[3][7][5][)]?[-\s\.]?[(]?[0-9]{2}?[)][-\s\.]?[0-9]{4,8}';
+
+    this.updateClient = this.formBuilder.group({
+      clientName: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5)]],
+      clientSoname: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5)]],
+      clientPhone: ['', [Validators.required, Validators.pattern(MOBILE_PATTERN)]],
+    });
+  }
 
   ngOnInit(): void {
     this.clientService.getClientById(this.data.id_client).subscribe(data=>{
